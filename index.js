66a93ef12159e7da33df9f56337fe084be9cc52d
nachodefuentes <idfs.afd@gmail.com>
2022-06-20 19:41:57 +0200

    ejercicios correctos

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var postI = 0;
+  
+  var postJ = 0;
+
+  for (let i = 0; i < 11; i++) {
+    postI+=postJ++
+}
+
+
+var sumaPares = 0;
+  for (let i = 0; i < 10; i++) {
+    if (i%2 == 0) {
+        sumaPares += i
+        
+    }
+  
+}
+
+
+var contarHasta10 = 0;
+do {
+    contarHasta10++
+    
+} while (contarHasta10 < 10);
+
+var sumaImpares= 0;
+for (let i = 0; i < 10; i++) {
+    if (i%2 !=0) {
+       sumaImpares += i
+    }
+    
+}
